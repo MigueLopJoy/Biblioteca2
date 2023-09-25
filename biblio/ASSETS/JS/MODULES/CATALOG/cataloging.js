57f62263481f7844c9ad2b9319a7947678f331f7
Miguel <m.lop.rod.97@gmail.com>
2023-09-25 19:55:19 +0200

    masmas

--- biblio/ASSETS/JS/MODULES/CATALOG/cataloging.js
+++ biblio/ASSETS/JS/MODULES/CATALOG/cataloging.js
@@ -1,9 +1,37 @@
 const d = document,
     currentPage = d.getElementById("cataloging-section"),
-    pageLinks = document.querySelectorAll(".page-link"),
-    pages = document.querySelectorAll(".page"),
-    searchAuthorForm = document.getElementById("search_author_form"),
-    createAuthorForm = document.getElementById("create_author_form");
+    pageLinks = d.querySelectorAll(".page-link"),
+    pages = d.querySelectorAll(".page"),
+    searchAuthorForm = d.querySelector(".form.search_author_form"),
+    createAuthorForm = d.querySelector(".form.create_author_form"),
+    searchBookworkForm = d.querySelector(".form.search_bookwork_form"),
+    createBookworkForm = d.querySelector(".form.create_authorbookwork_form"),
+    modal = d.getElementById("modal"),
+    resultsTable = d.getElementById("results-table"),
+    template = d.getElementById("template"),
+    fragment = d.createDocumentFragment();
+
+const authorTHead = 
+    `
+        <thead>
+            <th>Author id</td>
+            <th>First name</th>
+            <th>Last name</th>
+        </thead>
+    `,
+
+
+    bookworkTHead = 
+    `
+        <thead>
+            <th>Book work id</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Publication Year</th>
+        </thead>
+    `;
+
+
 
 
 pageLinks.forEach(pageLink => {
@@ -13,32 +41,49 @@ pageLinks.forEach(pageLink => {
     });
 });
 
+const showPage = pageOption => {
+    pages.forEach(page => {
+        page.classList.remove("active");
+    });
+    pageLinks.forEach(pageLink => {
+        pageLink.classList.remove("active");
+    })
+    d.querySelector(`.page-link.${pageOption}`).classList.toggle("active");
+    document.querySelector(`.page.${pageOption}`).classList.add("active");
+}
 
 d.addEventListener("submit", async e => {
 
     e.preventDefault();
 
-    if (e.target === createAuthorForm) {
+    let results;
+    if (e.target === searchAuthorForm) {
 
-        fetchRequest(
+        results = fetchRequest(
+            "GET",
+            joinParamsToURL(
+                "http://localhost:8080/authors-catalog/search-author",
+                {
+                    author_name: e.target.author_name.value
+                }
+            )
+        );
+    } else if (e.target === createAuthorForm) {
+
+        results = fetchRequest(
             "POST",
+            "http://localhost:8080/authors-catalog/save-author",
             {
                 firstName: e.target.firstName.value,
                 lastName: e.target.lastName.value
-            },
-            "http://localhost:8080/authors-catalog/save-author"
+            }
         );
-
-    } else if (e.target === searchAuthorForm) {
-
-        fetchGetRequest(
-            "http://localhost:8080/authors-catalog/search-author",
-            e.target.author_name.value);
     }
+    showResults(authorTHead, results);
 })
 
 
-const fetchRequest = async (method, bodyContent, url) => {
+const fetchRequest = async (method, url, bodyContent) => {
 
     try {
         let options = {
@@ -46,9 +91,7 @@ const fetchRequest = async (method, bodyContent, url) => {
             headers: {
                 "Content-type": "application/json; charset=utf-8"
             },
-            body: JSON.stringify(
-                bodyContent
-            )
+            body: bodyContent ? JSON.stringify(bodyContent) : null
         },
             res = await fetch(url, options),
             json = await res.json();
@@ -56,58 +99,63 @@ const fetchRequest = async (method, bodyContent, url) => {
         if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
         console.log(json)
+        return json;
 
     } catch (error) {
         console.log(error)
     }
 }
 
-const fetchGetRequest = async (url, params) => {
 
-    try {
-        let options = {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json; charset=utf-8"
-            }
-        },
-
-            res = await fetch(
-                getRequestURL(url, params),
-                options),
-
-            json = await res.json();
-
-        if (!res.ok) throw { status: res.status, statusText: res.statusText };
-
-        console.log(json)
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-const getRequestURL = (baseURL, params) => {
+const joinParamsToURL = (baseURL, params) => {
     let queryParams = Object.keys(params)
-        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(queryParams[key])}`)
-        .join('&'),
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .join('&');
 
-        completeURL = `${baseURL}?${queryParams}`;
-
-    return completeURL;
+    return `${baseURL}?${queryParams}`;
 }
 
-const showPage = pageId => {
-    pages.forEach(page => {
-        page.classList.remove("active");
-    });
-    pageLinks.forEach(pageLink => {
-        pageLink.classList.remove("active");
-    })
-    d.getElementsByName(pageId)[0].classList.toggle("active");
-    document.getElementById(pageId).classList.add("active");
+const showResults = (thead, searchResults) => {
+    modal.style.display = "flex";
+    modal.style.alignItems = "center";
+    modal.style.justifyContent = "center";
+    generaTableContent(thead, searchResults);
+}
+
+
+const generaTableContent = (thead, searchResults) => {
+
+    let tableContent = `${thead}<tbody>`;
+
+
+    for (let i = 0; i < searchResults.length; i++) {
+        tableContent += '<tr>';
+
+        Object.keys(searchResults[i])
+            .map(key => {
+                console.log(searchResults[i])
+                console.log(searchResults[key])
+                console.log(searchResults[i][key])
+
+                tableContent += 
+                    `
+                    <td>
+                        ${searchResults[i][key]}
+                    </td>
+                    `;
+            });
+        tableContent += '</tr>';
+    }
+
+    tableContent += '</tbody>';
+
+    resultsTable.innerHTML = tableContent;
+
+}
+
+const selectResult = () => {
+
 }
 
 
 showPage("author-page");
-
